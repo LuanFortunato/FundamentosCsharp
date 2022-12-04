61535e10f18040d8e50c835b85db81e0cd27578c
Luan Fortunato <luan.gustavo2004@gmail.com>
2022-12-04 18:27:04 -0300

    pagina web

---
+++ Fundamentos/CadastroProdutosWeb/Controllers/ProdutoController.cs
@@ -0,0 +1,35 @@
+﻿using CadastroProdutosWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroProdutosWeb.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public static List<Produto> ListaProdutos { get; set; } = new List<Produto>();
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Produto model)
+        {
+            ListaProdutos.Add(model);
+            ModelState.Clear();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return View(ListaProdutos);
+        }
+    }
+}
